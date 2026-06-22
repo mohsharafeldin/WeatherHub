4e35083ac26c3c65296d54eaa06130562bc96761
mohsharafeldin <mohamedkingj17@gmail.com>
2026-06-22 06:31:52 +0300

    creat WeatherHub project

---
+++ WeatherHub/WeatherHubApp.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherHubApp.swift
+//  WeatherHub
+//
+//  Created by mohamed sharaf on 20/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct WeatherHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Home()
+                
+        }
+    }
+}
